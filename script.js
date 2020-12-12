11681d8448ab7d9b955f5911e224a1fc6afa83ce
Christine Moore <cmoorelvt@gmail.com>
2020-12-12 12:35:02 -0500

    breaking things

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 
-$("img", "#gifs-rows").hover(function() {
-  $('.preset-file').toggle();
-  $('.gif-file').toggle();
-});
+// $("img", "#gifs-rows").hover(function() {
+//   $('.preset-file').toggle();
+//   $('.gif-file').toggle();
+// });
+
+// var staticGifSuffix = ".PNG";
+// var gifSuffix = ".gif";
+
+// $(document).ready(function() {
+
+//   $(".img-animate").each(function () {
+
+//      $(this).hover(
+//         function()
+//         {
+//             var originalSrc = $(this).attr("src");
+//             $(this).attr("src", originalSrc.replace(staticGifSuffix, gifSuffix));
+//         },
+//         function()
+//         {
+//             var originalSrc = $(this).attr("src");
+//             $(this).attr("src", originalSrc.replace(gifSuffix, staticGifSuffix));  
+//         }
+//      );
+
+//   });
+
+// });
